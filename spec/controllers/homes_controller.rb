3ce1f0c1c19b61262bdf2d8377ba91cf782f9382
Joshua Rudio <joshuarudio@gmail.com>
2017-10-03 15:47:55 -0700

    Write controller test for homepage and create homepage

---
+++ spec/controllers/homes_controller.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe HomesController do
+  describe 'GET #index' do
+    before(:each) { get :index }
+
+    it 'responds with a status code of 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the homes#index template' do
+      expect(response).to render_template(:index)
+    end
+    
+  end
+end
